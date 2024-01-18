fbd673bc1520bf7a048e4b3fdfa8d66a66783a5f
kuwacom <kuwa.com3@gmail.com>
2024-01-18 21:00:53 +0900

    update: バグの修正と配色の変更

--- src/exec.ts
+++ src/exec.ts
@@ -8,6 +8,18 @@
 
 import './assets/button.css';
 
+// const options = {
+//     type: 'basic',
+//     iconUrl: 'path/to/icon.png', // アイコンのパス
+//     title: '通知のタイトル',
+//     message: '通知の本文'
+//   };
+
+//   // 通知を表示
+//   chrome.notifications.create('notificationId', options, function(notificationId) {
+//     // 通知がクリックされたときの処理
+//   });
+
 window.addEventListener('load', () => {
     const modal = document.getElementById('modal');
     modal?.remove();
@@ -20,14 +32,22 @@ window.addEventListener('load', () => {
 
     execButtons.map((execButton) => {
         if (!execButton) return;
-        execButton.id = 'fuck';
+        execButton.id = '';
         execButton.className = 'einput';
     });
 
     if (execButtons[0]) { 
         execButtons[0].className += ' rainbow excuteB';
         //@ts-ignore
-        execButtons[0].value = '！！実行！！';
+        execButtons[0].value = '！実行！';
+        // execButtons[0].onclick = '';
+    }
+    if (execButtons[1]) { 
+        execButtons[1].className += ' redButton excuteB';
+    }
+    if (execButtons[2]) { 
+        execButtons[2].id = 'submitBtn';
+        execButtons[2].className += ' blueButton excuteB';
     }
 
     // const execPanel = document.getElementById('executePanel');
--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,4 @@ window.addEventListener('load', () => {
 
 });
 
-
 export { projectVersion }
